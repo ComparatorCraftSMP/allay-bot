d85719ee471ed1c1ae7672bd0586e10a487146da
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-05-19 19:15:25 +0000

    config

--- src/listeners/gpt/chat.ts
+++ src/listeners/gpt/chat.ts
@@ -2,6 +2,7 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener } from '@sapphire/framework';
 import { Message } from 'discord.js';
 import { OpenAI } from "langchain/llms/openai";
+import { config } from '../../config';
 
 
 
@@ -9,8 +10,13 @@ import { OpenAI } from "langchain/llms/openai";
 export class UserEvent extends Listener<typeof Events.MessageCreate> {
 	
 	public override async run(msg: Message) {
-		const model = new OpenAI({ openAIApiKey: process.env.OPENAI_KEY, temperature: 0.9, }, {basePath: process.env.OPENAI_BASEPATH});
-
+		const model = new OpenAI({ openAIApiKey: process.env.OPENAI_KEY, temperature: 0.9, modelName: 'gpt-3.5-turbo' }, {basePath: process.env.OPENAI_BASEPATH});
+		if (msg.channelId == config.channelId && msg.guildId == config.guildId && msg.author.id != this.container.client.user?.id) {
+		
+		const response = await model.call('Hello, how are you doing?').then((res) => console.log(res));
+		this.container.client.logger.info(response);
+		 await msg.reply('hi')
+		}
 
 	}
 }
