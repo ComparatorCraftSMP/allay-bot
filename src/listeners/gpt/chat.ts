2babd2b86543c51c7797f60e1b8a20d6027f4320
BossDaily <gboys2008@gmail.com>
2023-05-30 19:16:16 -0500

    stored data from github

--- src/listeners/gpt/chat.ts
+++ src/listeners/gpt/chat.ts
@@ -4,14 +4,9 @@ import { Message } from 'discord.js';
 import { OpenAI } from 'langchain/llms/openai';
 import { GithubRepoLoader } from 'langchain/document_loaders/web/github';
 import { config } from '../../config';
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import {
-  VectorStoreToolkit,
-  createVectorStoreAgent,
-  type VectorStoreInfo,
-} from "langchain/agents";
-
+import { HNSWLib } from 'langchain/vectorstores/hnswlib';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { VectorStoreToolkit, createVectorStoreAgent, type VectorStoreInfo } from 'langchain/agents';
 
 @ApplyOptions<Listener.Options>({ event: Events.MessageCreate })
 export class UserEvent extends Listener<typeof Events.MessageCreate> {
@@ -21,27 +16,35 @@ export class UserEvent extends Listener<typeof Events.MessageCreate> {
 			{ basePath: process.env.OPENAI_BASEPATH }
 		);
 		const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_KEY }, { basePath: process.env.OPENAI_BASEPATH });
-		const loader = new GithubRepoLoader('https://github.com/PaperMC/docs', { branch: 'main', recursive: false, unknown: 'warn' });
-		
+		const loader = new GithubRepoLoader('https://github.com/PaperMC/docs/tree/main/docs', {
+			branch: 'main',
+			recursive: false,
+			ignoreFiles: ['LICENSE', 'LICENSE-docs', '.yarn', '.devcontainer', 'config', 'src', '.gitignore']
+		});
+
 		const docs = await loader.load();
+		const vectorStore = await HNSWLib.fromDocuments(docs, embeddings);
 
-		const vectorStore = await HNSWLib.fromDocuments(docs, embeddings)
+		
 		const vectorStoreInfo: VectorStoreInfo = {
-			name: "PaperMC Docs",
-			description: "Documentation for PaperMC also known as Paper, a Minecraft server software",
+			name: 'PaperMC Docs',
+			description: 'Documentation for PaperMC also known as Paper, a Minecraft server software',
+			// @ts-ignore
 			vectorStore
-		}
+		};
 
-		const toolkit = new VectorStoreToolkit(vectorStoreInfo, model)
-		const agent = createVectorStoreAgent(model, toolkit)
+		const toolkit = new VectorStoreToolkit(vectorStoreInfo, model);
 
 		if (msg.channelId == config.channelId && msg.guildId == config.guildId && msg.author.id != this.container.client.user?.id) {
 			/* const response = await model.call(msg.content);
 			this.container.client.logger.info(response); */
-			const prompt = msg.content
-			const response = await agent.call({ prompt })
-
-			await msg.reply(response);
+			const agent = createVectorStoreAgent(model, toolkit);
+			const prompt = `${msg.content}`;
+			
+			
+			const response = await agent.call({ input: prompt });
+			console.log(response.output);
+			await msg.reply('balls');
 		}
 	}
 }
